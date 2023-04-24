77462e5582f8ef99205ac4cb76ce9f5f891e2228
galaxey56 <satyakiran56@gmail.com>
2023-04-25 00:19:24 +0530

    redis

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,9 +1,16 @@
 const Post = require("../models/posts");
 const io = require("../socket");
 const redis = require("redis");
-const client = redis.createClient("redis://red-ch3cr4rh4hsum42u29k0:6379");
+const client = redis.createClient();
 
-client.connect();
+client
+  .connect("redis://red-ch3cr4rh4hsum42u29k0:6379")
+  .then((result) => {
+    console.log("Redis Connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 exports.getPosts = async (req, res, next) => {
   const posts = await client.get("posts");
   let postsArray = await JSON.parse(posts);
@@ -44,7 +51,7 @@ exports.savePost = (req, res, next) => {
     .save()
     .then((result) => {
       io.getIO().emit("posts", { action: "create", post });
-      client.del("posts")
+      client.del("posts");
       res.send({
         sucess: true,
       });
@@ -57,16 +64,16 @@ exports.savePost = (req, res, next) => {
     });
 };
 
-exports.fetchFile = async(req, res, next) => {
+exports.fetchFile = async (req, res, next) => {
   const fileName = req.params.fileName;
-  const image = await client.get(fileName)
-  if(image === null){
-    Post.fetchFile(fileName, async(file) => {
-      await client.set(fileName, file)
+  const image = await client.get(fileName);
+  if (image === null) {
+    Post.fetchFile(fileName, async (file) => {
+      await client.set(fileName, file);
       res.send(file);
     });
-  }else{
-    res.send(image)
+  } else {
+    res.send(image);
   }
 };
 
